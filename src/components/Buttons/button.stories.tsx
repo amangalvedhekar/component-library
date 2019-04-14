e0d50db2fe2e3b52bc858e8d5ae78a067b8376e3
Anirudh <amangalvedhekar@icloud.com>
2019-04-14 03:41:13 -0400

    add info addons

--- src/components/Buttons/button.stories.tsx
+++ src/components/Buttons/button.stories.tsx
@@ -1,10 +1,14 @@
+import { checkA11y } from '@storybook/addon-a11y';
+import { withInfo } from '@storybook/addon-info';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import Button from "./Button";
 storiesOf("Button", module)
+    .addDecorator(checkA11y)
+    .addDecorator(withInfo)
     .add("with text", () => (
         <Button>Hello Button</Button>
     ))
     .add("with some emoji", () => (
         <Button>😀 😎 👍 💯</Button>
-    ));
+    ),{ info: { inline: true, header: false } });
