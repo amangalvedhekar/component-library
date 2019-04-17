ed6d163f424b21c5b2127e532cf9087588fbadbd
Anirudh <amangalvedhekar@icloud.com>
2019-04-17 17:56:58 -0400

    unused files deleted

--- src/components/Card/CardBody.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import {ICardBody} from "./types";
-
-const CardBody: React.FunctionComponent<ICardBody> = props => (
-    <div>{props.children}</div>
-);
-
-export default CardBody;
